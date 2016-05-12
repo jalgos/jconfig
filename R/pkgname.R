ea677d0328ef622de7826fa856b121fa4908c4c9
Sébastien Lamy de la Chapelle <sebastien@jalgos.com>
2016-05-12 15:37:14 +0200

    Documented and built

---
+++ R/pkgname.R
@@ -0,0 +1,7 @@
+#' jconfig: Managing configuration files
+#'
+#' This package provides a way to manage configuration files. Files can be loaded from disk. They can be either JSON or R. Configurations can include other configuration files with clauses of the form: "include": "path/to/other/config"
+#' 
+#' @section Functions:
+#' Users need to use 'loadJConfig', 'get.config' and 'set.config'
+"_PACKAGE"
